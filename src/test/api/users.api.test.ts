51bfa0cf2697216a668767a50026c4a964cc6db1
Isaac Mogano <isaacm@hopned.co.za>
2024-07-09 11:25:05 +0200

    Updated test scripts useed to test user api

--- src/test/api/users.api.test.ts
+++ src/test/api/users.api.test.ts
@@ -1,35 +1,123 @@
-import {describe, test} from '@jest/globals';
+import {describe, expect, test} from '@jest/globals';
+import axios, { AxiosError } from 'axios';
 
-// TODO: Manage Users
-// TODO: should get users - users does exist
-// TODO: should get users - users does not exist
-// TODO: should add user - does not exist
-// TODO: should add user - does exist
-// TODO: should get user - does exit exist
-// TODO: should get user - does not exist
-// TODO: should update user - does exist
-// TODO: should update user - does not exist
-// TODO: should delete user - does exist
-// TODO: should delete user - does not exist  
 describe('Manage users - api level', () => {
-  test('should not get users - user unauthorized',()=>{})
-  test('should get users - schema invalid',()=>{})
-  test('should get users - users does not exist',()=>{})
-  test('should get users - users does exist',()=>{})
+  test('should not register user - schema invalid',async ()=>{
+    try {
+      const invalidBody = {
+        name:"TestName",
+        surname:"Surname",
+        email:"testuser@gmail.com", 
+      };
+  
+      await axios.post("http://127.0.0.1:5000/user/register", {
+        ...invalidBody
+      })
+    } catch (error: Error | AxiosError| any) {
+      if (error instanceof AxiosError) {
+        expect(error.response?.status).toBe(422);
+        expect(error.response?.data.message).toBe("Api schema validation failed. Please find taskify-much documentation!");
+      }
+    }
+  })
+  test('should register user - user email does not exist',async ()=>{
+    const validBody = {
+      name:"TestName",
+      surname:"Surname",
+      email:"testuser@gmail.com", 
+      password: "12345@agdjjd"
+    };
 
-  test('should not add user - schema invalid',()=>{})
-  test('should add user - user email does not exist',()=>{})
-  test('should add user - user email does exist',()=>{})
+    const result = await axios.post("http://127.0.0.1:5000/user/register", {
+      ...validBody
+    });
+    expect(result.data.name).toBe(validBody.name);
+    expect(result.data.email).toBe(validBody.email);
+    expect(result.data.surname).toBe(validBody.surname);
+    expect(result.status).toBe(200);
+  })
 
-  test('should not get delete user - schema invalid',()=>{})
-  test('should get user - user does exist',()=>{})
-  test('should get user - users does not exist',()=>{})
+  test('should not register user - user email does exist',async ()=>{
+    try {
+      const validBody = {
+        name:"TestName",
+        surname:"Surname",
+        email:"testuser@gmail.com", 
+        password: "12345@agdjjd"
+      };
+  
+      await axios.post("http://127.0.0.1:5000/user/register", {
+        ...validBody
+      });      
+    } catch (error: Error | AxiosError| any) {
+      if (error instanceof AxiosError) {
+        expect(error.response?.status).toBe(400);
+        expect(error.response?.data.message).toBe("User profile exist already.");
+      }
+    }
+  })
 
-  test('should not update delete user - schema invalid',()=>{})
-  test('should update user - users does not exist',()=>{})
-  test('should update user - users does exist',()=>{})
+  test('should not log user in - schema invalid',async ()=>{
+    try {
+      const invalidBody = {
+        Email:"testuser@gmail.com", 
+        Password: "12345@agdjjd"
+      };
+  
+      await axios.post("http://127.0.0.1:5000/user/login", {
+        ...invalidBody
+      })
+    } catch (error: Error | AxiosError| any) {
+      if (error instanceof AxiosError) {
+        expect(error.response?.status).toBe(422);
+        expect(error.response?.data.message).toBe("Api schema validation failed. Please find taskify-much documentation!");
+      }
+    }
+  })
+  test('should not log user in - invalid credentials (password incorrect)',async ()=>{
+    try {
+      const invalidBody = {
+        email:"testuser@gmail.com", 
+        password: "12345@agdjjd"
+      };
+  
+      await axios.post("http://127.0.0.1:5000/user/login", {
+        ...invalidBody
+      })
+    } catch (error: Error | AxiosError| any) {
+      if (error instanceof AxiosError) {
+        expect(error.response?.status).toBe(401);
+        expect(error.response?.data.message).toBe("Invalid password.");
+      }
+    }
+  })
+  test('should not log user in - invalid credentials (email incorrect)',async ()=>{
+    try {
+      const invalidBody = {
+        email:"!testuser@gmail.com", 
+        password: "12345@agdjjd"
+      };
+  
+      await axios.post("http://127.0.0.1:5000/user/login", {
+        ...invalidBody
+      })
+    } catch (error: Error | AxiosError| any) {
+      if (error instanceof AxiosError) {
+        expect(error.response?.status).toBe(401);
+        expect(error.response?.data.message).toBe("Invalid user credentials.");
+      }
+    }
+  })
+  test('should log user in - credentials valid',async ()=>{
+    const validBody = {
+      email:"testuser@gmail.com", 
+      password: "12345@agdjjd"
+    };
 
-  test('should not delete user - schema invalid',()=>{})
-  test('should delete user - users does exist',()=>{})
-  test('should get user - users does not exist',()=>{})
+    const result = await axios.post("http://127.0.0.1:5000/user/login", {
+      ...validBody
+    })
+    expect(result?.status).toBe(200);
+    expect(result?.data.token).toBeTruthy();
+  })
 })
