4503a70088210030c1406859cdacb2b043c04432
Isaac Mogano <isaacm@hopned.co.za>
2024-07-10 19:23:53 +0200

    Added file to overide  fastify as part of fastify/jwt setup

---
+++ types.d.ts
@@ -0,0 +1,15 @@
+import 'fastify';
+import { FastifyJwtNamespace } from '@fastify/jwt';
+import User from './src/database/models/User';
+
+declare module 'fastify' {
+  interface FastifyRequest {
+    user?: User;
+    startTime: number;
+  }
+  
+  interface FastifyInstance extends 
+    FastifyJwtNamespace<{namespace: 'security'}> {
+  }
+}
+
