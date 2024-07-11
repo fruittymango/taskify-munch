8caffdf50040b3b0ff9db9fc377f354f4509ad22
Isaac Mogano <isaacm@hopned.co.za>
2024-07-11 11:03:28 +0200

    Corrected TaskAssignmentsController spelling and added removed service functions not used anymore

--- src/api/routes/task_assignments.route.ts
+++ src/api/routes/task_assignments.route.ts
@@ -1,11 +1,11 @@
 import authMiddleware from "../middleware/authentication.middleware";
 import { FastifyInstance } from "fastify/types/instance";
-import { TasAssignmentsController } from "../controllers/task_assignments.controller";
+import { TaskAssignmentsController } from "../controllers/task_assignments.controller";
 import { TaskAssignmentSchema } from "../schemas/task_assignments.schema";
 
 async function taskAssignRoutes(fastify: FastifyInstance){
-  fastify.post("/:guid", {schema: TaskAssignmentSchema, preHandler: authMiddleware}, TasAssignmentsController.AddTaskAssignment);
-  fastify.delete("/:guid", {schema: TaskAssignmentSchema, preHandler: authMiddleware}, TasAssignmentsController.DeleteTaskAssignment);
+  fastify.post("/:guid", {schema: TaskAssignmentSchema, preHandler: authMiddleware}, TaskAssignmentsController.AddTaskAssignment);
+  fastify.delete("/:guid", {schema: TaskAssignmentSchema, preHandler: authMiddleware}, TaskAssignmentsController.DeleteTaskAssignment);
 }
 
 export default taskAssignRoutes;
--- src/api/services/taskAssignment.service.ts
+++ src/api/services/taskAssignment.service.ts
@@ -8,32 +8,6 @@ export const createTaskAssignments = async (payload: TaskAssignmentInput): Promi
     return taskAssignment[0];
 }
 
-export const getTaskAssignmentsByTaskId = async (userTaskId: number): Promise<TaskAssignments[]> => {
-    const taskAssignment = await TaskAssignments.findAll({
-        where:{
-            taskId: userTaskId
-        }, paranoid:false
-    })
-    if (!taskAssignment || taskAssignment.length < 1) {
-        // @todo throw custom error
-        throw new NotFoundError('Task assignments not found')
-    }
-    return taskAssignment
-}
-
-export const getTaskAssignmentsByTaskIdUserId = async (userId: number, taskId:number): Promise<TaskAssignments> => {
-    const taskAssignment = await TaskAssignments.findOne({
-        where:{
-            taskId, userId
-        }, paranoid:false
-    })
-    if (!taskAssignment) {
-        // @todo throw custom error
-        throw new NotFoundError('Task assignment not found');
-    }
-    return taskAssignment
-}
-
 export const deleteTaskAssignmentsByUserIdTaskId = async (userId: number, taskId: number): Promise<boolean> => {
     const deletedTaskAssignmentsCount = await TaskAssignments.destroy({
         where: {taskId, userId}
