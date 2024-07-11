3455daf341c3d0d0eab1e88a7fcf35dcad4e306e
Isaac Mogano <isaacm@hopned.co.za>
2024-07-11 08:13:51 +0200

    Added error mapping classes to make it easier to deal with errors and status codes

---
+++ src/helpers/errors.ts
@@ -0,0 +1,60 @@
+export class BadRequestError extends Error {
+    statusCode: number;
+  
+    constructor(message: string) {
+      super(message);
+      this.statusCode = 400;
+      this.name = 'BadRequestError';
+    }
+  }
+  
+  export class ValidationError extends Error {
+    statusCode: number;
+  
+    constructor(message: string) {
+      super(message);
+      this.statusCode = 422;
+      this.name = 'ValidationError';
+    }
+  }
+
+  export class RateLimtError extends Error {
+    statusCode: number;
+  
+    constructor(message: string) {
+      super(message);
+      this.statusCode = 429;
+      this.name = 'RateLimitError';
+    }
+  }
+  
+  export class NotFoundError extends Error {
+    statusCode: number;
+  
+    constructor(message: string) {
+      super(message);
+      this.statusCode = 404;
+      this.name = 'NotFoundError';
+    }
+  }
+  
+  export class InternalServerError extends Error {
+    statusCode: number;
+  
+    constructor(message: string) {
+      super(message);
+      this.statusCode = 500;
+      this.name = 'InternalServerError';
+    }
+  }
+
+  export class AuthError extends Error {
+    statusCode: number;
+  
+    constructor(message: string) {
+      super(message);
+      this.statusCode = 401;
+      this.name = 'AuthError';
+    }
+  }
+  
