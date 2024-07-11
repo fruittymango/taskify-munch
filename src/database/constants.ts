358cc431faf7a67c42e7248f0625304215c75777
Isaac Mogano <isaacm@hopned.co.za>
2024-07-11 08:09:23 +0200

    Added constants for labels, priorities and statuses

---
+++ src/database/constants.ts
@@ -0,0 +1,24 @@
+export default {
+    priorities: [
+        {title: "Low"},
+        {title: "Medium"},
+        {title: "High"},
+    ],
+    labels: [
+        {title: "Bug"},
+        {title: "Awaiting Approval"},
+        {title: "Awaiting Integration"},
+        {title: "No Designs"},
+        {title: "API"},
+        {title: "Approved"},
+        {title: "Ready"},
+        {title: "UI"},
+    ],
+    statuses: [
+        {title: "Todo"},
+        {title: "Open"},
+        {title: "Completed"},
+        {title: "Backlog"},
+        {title: "Testing"},
+    ]
+}
