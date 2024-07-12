3d37ed657a9f90770934ca9f8bf4d2bbfe499c52
Isaac Mogano <isaacm@hopned.co.za>
2024-07-12 15:03:06 +0200

    Added DatabaseRelatedError to catch all db service related exceptions

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -1,59 +1,69 @@
 export class BadRequestError extends Error {
-  statusCode: number;
+    statusCode: number;
 
-  constructor(message: string) {
-    super(message);
-    this.statusCode = 400;
-    this.name = 'BadRequestError';
-  }
+    constructor(message: string) {
+        super(message);
+        this.statusCode = 400;
+        this.name = "BadRequestError";
+    }
 }
 
 export class ValidationError extends Error {
-  statusCode: number;
+    statusCode: number;
 
-  constructor(message: string) {
-    super(message);
-    this.statusCode = 422;
-    this.name = 'ValidationError';
-  }
+    constructor(message: string) {
+        super(message);
+        this.statusCode = 422;
+        this.name = "ValidationError";
+    }
 }
 
-export class RateLimtError extends Error {
-  statusCode: number;
+export class RateLimitError extends Error {
+    statusCode: number;
 
-  constructor(message: string) {
-    super(message);
-    this.statusCode = 429;
-    this.name = 'RateLimitError';
-  }
+    constructor(message: string) {
+        super(message);
+        this.statusCode = 429;
+        this.name = "RateLimitError";
+    }
 }
 
 export class NotFoundError extends Error {
-  statusCode: number;
+    statusCode: number;
 
-  constructor(message: string) {
-    super(message);
-    this.statusCode = 404;
-    this.name = 'NotFoundError';
-  }
+    constructor(message: string) {
+        super(message);
+        this.statusCode = 404;
+        this.name = "NotFoundError";
+    }
 }
 
 export class InternalServerError extends Error {
-  statusCode: number;
+    statusCode: number;
 
-  constructor(message: string) {
-    super(message);
-    this.statusCode = 500;
-    this.name = 'InternalServerError';
-  }
+    constructor(message: string) {
+        super(message);
+        this.statusCode = 500;
+        this.name = "InternalServerError";
+    }
 }
 
 export class AuthError extends Error {
-  statusCode: number;
+    statusCode: number;
 
-  constructor(message: string) {
-    super(message);
-    this.statusCode = 401;
-    this.name = 'AuthError';
-  }
+    constructor(message: string) {
+        super(message);
+        this.statusCode = 401;
+        this.name = "AuthError";
+    }
+}
+
+export class DatabaseRelatedError extends Error {
+    statusCode: number;
+
+    constructor(message: string) {
+        super(message);
+        this.statusCode = 422;
+        this.name = "DatabaseRelatedError";
+    }
 }
