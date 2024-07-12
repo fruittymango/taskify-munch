efaccae8da3c467fdc142531eaa47423ed8086a6
Isaac Mogano <isaacm@hopned.co.za>
2024-07-12 15:00:23 +0200

    Added find by title function

--- src/api/services/priority.service.ts
+++ src/api/services/priority.service.ts
@@ -1,10 +1,34 @@
-import Priority, { PriorityInput } from '../../database/models/Priority';
+import Priority, { PriorityInput } from "../../database/models/Priority";
+import { DatabaseRelatedError, NotFoundError } from "../../helpers/errors";
 
 export const getAllPriorities = async (): Promise<Priority[]> => {
-    return await Priority.findAll();
-}
+    try {
+        return await Priority.findAll();
+    } catch (error) {
+        throw new DatabaseRelatedError("Failed to get available priorities.");
+    }
+};
 
-export const addBulkPriorities = async (payload: PriorityInput[]): Promise<Priority[]> => {
-    const priorities = await Priority.bulkCreate(payload)
-    return priorities
-} 
+export const findPriorityByTitle = async (title: string): Promise<Priority> => {
+    try {
+        console.log(title);
+        const priority = await Priority.findOne({ where: { title } });
+        if (!priority) {
+            throw new NotFoundError("Priority not found");
+        }
+        return priority;
+    } catch (error) {
+        throw new DatabaseRelatedError("Failed to find priority by the title.");
+    }
+};
+
+export const addBulkPriorities = async (
+    payload: PriorityInput[]
+): Promise<Priority[]> => {
+    try {
+        const priorities = await Priority.bulkCreate(payload);
+        return priorities;
+    } catch (error) {
+        throw new DatabaseRelatedError("Failed to add bulk priorities.");
+    }
+};
--- src/api/services/statuses.service.ts
+++ src/api/services/statuses.service.ts
@@ -1,11 +1,33 @@
-import Status, { StatusInput } from '../../database/models/Status';
-import { NotFoundError } from '../../helpers/errors';
+import Status, { StatusInput } from "../../database/models/Status";
+import { DatabaseRelatedError, NotFoundError } from "../../helpers/errors";
 
 export const getAllStatuses = async (): Promise<Status[]> => {
-    return Status.findAll();
-}
+    try {
+        return Status.findAll();
+    } catch (error) {
+        throw new DatabaseRelatedError("Failed to get available statuses.");
+    }
+};
 
-export const addBulkStatuses = async (payload: StatusInput[]): Promise<Status[]> => {
-    const labels = await Status.bulkCreate(payload)
-    return labels
-} 
+export const findStatusByTitle = async (title: string): Promise<Status> => {
+    try {
+        const status = await Status.findOne({ where: { title } });
+        if (!status) {
+            throw new NotFoundError("Status not found");
+        }
+        return status;
+    } catch (error) {
+        throw new DatabaseRelatedError("Failed to find status by title.");
+    }
+};
+
+export const addBulkStatuses = async (
+    payload: StatusInput[]
+): Promise<Status[]> => {
+    try {
+        const labels = await Status.bulkCreate(payload);
+        return labels;
+    } catch (error) {
+        throw new DatabaseRelatedError("Failed to add bulk statuses.");
+    }
+};
