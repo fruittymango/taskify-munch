5a8702b0c7cbed10b03871821fb2d84cf25e226c
Isaac Mogano <isaacm@hopned.co.za>
2024-07-12 18:22:05 +0200

    Updated the global types for the Fastify request

--- types.d.ts
+++ types.d.ts
@@ -1,18 +1,12 @@
 import "fastify";
 import { FastifyJwtNamespace } from "@fastify/jwt";
-import { FastifyRequest } from "fastify/types/request";
 import User from "./src/database/models/User";
 
-type UserRequest = {
-    user?: User;
-    startTime: number;
-};
-
-declare module "fastify/types/request" {
-    interface FastifyRequest extends UserRequest<> {}
-}
 declare module "fastify" {
-    interface FastifyRequest extends UserRequest<> {}
+    interface FastifyRequest {
+        user?: User;
+        startTime: number;
+    }
 
     interface FastifyInstance
         extends FastifyJwtNamespace<{ namespace: "security" }> {}
